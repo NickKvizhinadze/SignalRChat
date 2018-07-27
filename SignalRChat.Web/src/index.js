c93930acf7d0c3f3cfa825962f833b77926079e3
nickkvizhinadze <nickkvizhinadze@gmail.com>
2018-07-27 09:19:27 +0400

    Web => Added bootstrap to project

--- SignalRChat.Web/src/index.js
+++ SignalRChat.Web/src/index.js
@@ -1,9 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Router from './Router';
 import store from './store';
+
 
 ReactDOM.render(<Router store={store} />, document.getElementById('root'));
 registerServiceWorker();
