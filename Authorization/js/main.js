356ab57f6599b99edc641b927f685b9f43b5d56f
Giorgi Gurgenashvili <giogurgena@gmail.com>
2018-08-04 17:27:12 +0400

    hover fixes

--- Authorization/js/main.js
+++ Authorization/js/main.js
@@ -6,26 +6,36 @@ const loginheader = document.querySelector('.loginheader');
 const signupform = document.querySelector('.signup-form');
 const signupheader = document.querySelector('.signupheader');
 
+container.classList.add('hover-login');
+loginform.classList.add('show');
+loginheader.classList.add('up');
+
 login.addEventListener('mouseenter', () => {
     container.classList.add('hover-login');
     loginform.classList.add('show');
     loginheader.classList.add('up');
+    container.classList.remove('hover-signup');
+    signupform.classList.remove('show');   
+    signupheader.classList.remove('up');
 });
 
-login.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-login');
-    loginform.classList.remove('show');
-    loginheader.classList.remove('up');
-});
+// login.addEventListener('mouseleave', () => {
+//     container.classList.remove('hover-login');
+//     loginform.classList.remove('show');
+//     loginheader.classList.remove('up');
+// });
 
 signup.addEventListener('mouseenter', () => {
     container.classList.add('hover-signup');
     signupform.classList.add('show');
     signupheader.classList.add('up');
+    container.classList.remove('hover-login');
+    loginform.classList.remove('show');
+    loginheader.classList.remove('up');
 });
 
-signup.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-signup');
-    signupform.classList.remove('show');   
-    signupheader.classList.remove('up');
-});
+// signup.addEventListener('mouseleave', () => {
+//     container.classList.remove('hover-signup');
+//     signupform.classList.remove('show');   
+//     signupheader.classList.remove('up');
+// });
